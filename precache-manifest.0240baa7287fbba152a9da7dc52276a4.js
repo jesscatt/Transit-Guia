87b1371dfc6f4316787e84d8c7ea852b68fca03f
Jessika Rodrigues <85262133+jesscatt@users.noreply.github.com>
2024-09-12 17:45:27 +0000

    Updates

---
+++ precache-manifest.0240baa7287fbba152a9da7dc52276a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b90578f51f211c449202c5153e99889",
+    "url": "/Transit-Guia/index.html"
+  },
+  {
+    "revision": "cee4464b1eb87a1dd7ad",
+    "url": "/Transit-Guia/static/css/main.442fc5ae.chunk.css"
+  },
+  {
+    "revision": "a39697a6fb9eac28a6df",
+    "url": "/Transit-Guia/static/js/2.a1deadc3.chunk.js"
+  },
+  {
+    "revision": "cee4464b1eb87a1dd7ad",
+    "url": "/Transit-Guia/static/js/main.35665967.chunk.js"
+  },
+  {
+    "revision": "9cb701e9f4bbdd1bc7dd",
+    "url": "/Transit-Guia/static/js/runtime~main.1f7f6cc9.js"
+  }
+]);
